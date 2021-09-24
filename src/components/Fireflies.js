195a6a2eb073e57d38db34e4214a3412951305bf
Khongchai greesuradej <world1955@hotmail.com>
2021-09-24 15:59:25 +0700

    particles only

--- src/components/Fireflies.js
+++ src/components/Fireflies.js
@@ -16,10 +16,6 @@ const r = () => Math.max(0.2, Math.random());
  */
 const Fatline = ({ curve, width, color }) => {
   const material = useRef();
-  useFrame(
-    //change the offset value of the dash, without this, the fireflies would not be moving.
-    (_, delta) => (material.current.uniforms.dashOffset.value -= delta / 100)
-  );
   return (
     <mesh>
       {/* @ts-ignore */}
@@ -36,7 +32,7 @@ const Fatline = ({ curve, width, color }) => {
         dashArray={0.1}
         //How much of a dash would you like to see? ratio of 0 = straight line
         //ratio of 1 = invisible
-        dashRatio={0.99}
+        dashRatio={0}
       />
     </mesh>
   );
@@ -45,41 +41,33 @@ const Fatline = ({ curve, width, color }) => {
 export default function Fireflies({ count, colors, radius = 10 }) {
   const lines = useMemo(() => {
     let paths = [];
-    for (let i = 0; i < count; i++) {
-      //Get starting position with a bit of variations.
-      const startingPos = new THREE.Vector3(Math.sin(0) * radius * r(), Math.cos(0) * radius * r(), 0);
-      //For each of starting positions, attach thirty more points in a circular path.
-      const dots = 30;
+    const pathsCount = 10;
+    const dotsCount = 20;
 
-      //Refactored from the commented code below for readability
-      const points = [];
-      for (let angle = 0; angle < (Math.PI * 2); angle += (Math.PI * 2) / 30){
-        const xAngle = Math.sin(angle) * radius * r();
-        const yAngle = Math.cos(angle) * radius * r();
-        const zAngle = 0;
-        points.push(startingPos.add(new THREE.Vector3(xAngle, yAngle, zAngle)).clone());
-      }
-      // const points = new Array(dots).fill().map((_, index) => {
-      //   //Evenly distribute the 30 dots in a circle (half circle would be just Math.PI)
-      //   const angle = (index / dots) * Math.PI * 2;
-      //   //Start each subsequent dots after the prev one.
-      //   return startingPos
-      //     .add(new THREE.Vector3(Math.sin(angle) * radius * r(), Math.cos(angle) * radius * r(), 0))
-      //     .clone();
-      // });
+    for (let i = 0; i < pathsCount; i++){
+        const startingPos = new THREE.Vector3(Math.sin(0) * r() * radius, Math.cos(0) * r() * radius, 0);
+        const points = [];
 
-      //Draw spline between the 30 points.
-      //getPoints() specifies how many points from the interpolated splines you want.
-      const curve = new THREE.CatmullRomCurve3(points).getPoints(100);
-      //info for the path
-      const path = {
-        color: colors[parseInt(colors.length * Math.random())],
-        width: Math.max(1.6, (2 * i) / 10),
-        curve,
-      };
-      paths.push(path);
+        for (let angle = 0; angle < (Math.PI * 2); angle += (Math.PI * 2) / dotsCount){
+            const x = Math.sin(angle) * radius * r();
+            const y = Math.cos(angle) * radius * r();
+            const z = 0;
+
+            points.push(startingPos.add(new THREE.Vector3(x, y, z)).clone());
+        }
+        const arcSegments = 100;
+        const curve = new THREE.CatmullRomCurve3(points).getPoints(arcSegments);
+
+        const path = {
+              color: colors[parseInt(colors.length * Math.random())],
+              width: Math.max(1.6, (2 * i) / 10),
+              curve,
+            };
+
+        paths.push(path);
     }
     return paths;
+      
   }, [count, radius, colors]);
 
   return (
